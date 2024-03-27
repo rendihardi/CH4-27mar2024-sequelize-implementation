966fac487e16b9faea22df390deecdabeb1368a4
rendihardi <rendirenhard@gmail.com>
2024-03-28 00:16:04 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+
+const morgan = require("morgan");
+
+const router = require("./routes");
+
+const PORT = process.env.PORT || "3000";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+
+app.use(morgan("dev"));
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port : ${PORT}`);
+});
